e8ac0cba6ab42e83c13b7e7d7685d8eca4a54500
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-01-13 09:30:21 -0700

    Create #expense method - department.rb

--- lib/department.rb
+++ lib/department.rb
@@ -1,13 +1,19 @@
 class Department
 	attr_reader :name,
-							:employees
+							:employees,
+              :expenses
 	
 	def initialize(name)
 		@name = name
 		@employees = []
+    @expenses = 0
 	end
 
   def hire(employee)
     @employees.push(employee)
+  end
+
+  def expense(amount)
+    @expenses += amount
   end
 end
