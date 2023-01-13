70fbccfea1ec7056c28ff439fba6fd1041944b7d
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-01-13 09:22:59 -0700

    Create #initialize method - department.rb

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -2,7 +2,7 @@ require './spec/spec_helper'
 
 RSpec.describe Department do
 
-  let(:customer_service) { Department.new("Customer Service") }
+  let(:department) { Department.new("Customer Service") }
   let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"}) }
   let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) }
 
