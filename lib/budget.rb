d1daa2d4cd9a2e096768b8febf6a26d9881a5dda
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-01-13 11:29:24 -0700

    Create #employee_salaries - budget.rb

--- lib/budget.rb
+++ lib/budget.rb
@@ -5,6 +5,7 @@ class Budget
 	def initialize(year)
 		@year = year
     @departments = []
+    @employee_salaries = {}
 	end
 
   def add_department(department)
@@ -15,5 +16,16 @@ class Budget
     @departments.find_all do |department|
       department.expenses < 500
     end
+  end
+
+  def employee_salaries
+    employee_salaries = Hash.new { |dpt, key| dpt[key] = [] }
+    @departments.each do |department|
+      department.employees.each do |employee|
+        employee_salaries[department] = {name: employee.name, salary: employee.salary} 
+      end
+    end
+    
+    employee_salaries
   end
 end
