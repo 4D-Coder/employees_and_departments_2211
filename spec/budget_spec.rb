90e7ff90faa526dc444c54ff96a83ef25ff8d18e
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-01-13 09:44:58 -0700

    Create #add_department test - budget_spec.rb

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -1,6 +1,9 @@
 require './spec/spec_helper'
 
 RSpec.describe Budget do
+  let(:department_1) { Department.new("Customer Service") }
+  let(:department_2) { Department.new("Human Resources") }
+
   let(:budget) { Budget.new('2023') }
 
   describe 'Iteration 3' do
@@ -12,7 +15,19 @@ RSpec.describe Budget do
       
       it 'has readable attributes' do
         expect(budget.year).to eq('2023')
+        expect(budget.departments).to eq([])
 			end
     end
+    
+    context '#add_department' do
+	    it 'can list all of its departments' do
+        expect(budget.departments).to eq([])
+
+        budget.add_department(department_1)
+        budget.add_department(department_2)
+				
+
+        expect(budget.departments).to eq([department_1, department_2])
+			end
   end
 end
